367526d18be7a78f41e1f1de81bb90ca4608b9d4
Bjorn Tipling <bjorn@ambientchill.com>
2016-11-24 18:35:41 -0800

    seperate hello worlds for client and server

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,45 @@ const gulp = require('gulp');
 const rollup = require('gulp-rollup');
 
 const paths = {
-  JS_SRC: './src/js/**/*.js',
-  JS_ENTRY: './src/js/main.js',
+  js: {
+    client: {
+      src:  './src/js/client/**/*.js',
+      entry: './src/js/client/main.js',
+      dest: './dist/client/',
+    },
+    server: {
+      src:  './src/js/server/**/*.js',
+      entry: './src/js/server/main.js',
+      dest: './dist/server/',
+    },
+  },
 };
 
 const tasks = {
-  JS: 'js_build',
+  JS_CLIENT: 'js_client',
+  JS_SERVER: 'js_server',
   DEFAULT: 'default',
 };
 
-gulp.task(tasks.JS, function() {
-  return gulp.src(paths.JS_SRC)
+gulp.task(tasks.JS_CLIENT, function() {
+  return gulp.src(paths.js.client.src)
     .pipe(rollup({
-      entry: paths.JS_ENTRY,
+      entry: paths.js.client.entry,
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(paths.js.client.dest));
+});
+
+gulp.task(tasks.JS_SERVER, function() {
+  return gulp.src(paths.js.server.src)
+    .pipe(rollup({
+      entry: paths.js.server.entry,
+    }))
+    .pipe(gulp.dest(paths.js.server.dest));
 });
 
 const ALL_TASKS = [
-  tasks.JS,
+  tasks.JS_CLIENT,
+  tasks.JS_SERVER,
 ];
 
 gulp.task(tasks.DEFAULT, gulp.parallel(ALL_TASKS));
